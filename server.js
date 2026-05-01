3a1c9589f97cc9ff1d754414e71c0ada0fd29b36
Ahmed Selim <ahmedselimnoshi@gmail.com>
2026-05-01 17:34:31 +0300

    Add db_username in config.env for better encapsulation of private data

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './config.env' });
 
-const app = require("./app");
+const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  "<db_password>",
-  process.env.DATABASE_PASSWORD
+const DB = process.env.DATABASE.replace('<db_username>', process.env.DATABASE_USERNAME).replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
 );
+
+console.log(DB.replace(process.env.DATABASE_PASSWORD, 'HIDDEN'));
 
 mongoose
   .connect(DB)
-  .then(() => console.log("DB connection successful!"));
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log('DB connection FAILED!!', err));
+
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
