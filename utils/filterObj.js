7e4008b0cbd555497bf03aa30c64609e43af3ca5
Ahmed Selim <ahmedselimnoshi@gmail.com>
2026-05-01 18:15:26 +0300

    Add filterObj

---
+++ utils/filterObj.js
@@ -0,0 +1,11 @@
+module.exports = (obj , ...allowedFields) => {
+    const newObj = {};
+
+    Object.keys(obj).forEach(field => {
+        if(allowedFields.includes(field)){
+            newObj[field] = obj[field]
+        }
+    });
+
+    return newObj;
+};
