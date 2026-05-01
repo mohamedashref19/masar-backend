9900194f72d7d0c9e1155593f8e9cba97cc5136b
Ahmed Selim <ahmedselimnoshi@gmail.com>
2026-05-02 01:42:56 +0300

    Add active field to soft delete users

--- models/userModel.js
+++ models/userModel.js
@@ -3,160 +3,154 @@ const mongoose = require('mongoose');
 
 // modules
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 const freelancerProfileSchema = new mongoose.Schema(
-  {
-    title: String,
+    {
+        title: String,
 
-    bio: String,
+        bio: String,
 
-    skills: [String],
+        skills: [String],
 
-    experienceLevel: {
-      type: String,
-      enum: ['beginner', 'intermediate', 'expert'],
-      default: 'beginner',
+        experienceLevel: {
+            type: String,
+            enum: ['beginner', 'intermediate', 'expert'],
+            default: 'beginner',
+        },
+
+        portfolioLinks: [String],
+
+        githubLink: String,
+
+        hourlyRate: Number,
+
+        rating: {
+            type: Number,
+            default: 0,
+        },
     },
 
-    portfolioLinks: [String],
-
-    githubLink: String,
-
-    hourlyRate: Number,
-
-    rating: {
-      type: Number,
-      default: 0,
-    },
-  },
-
-  { _id: false },
+    { _id: false },
 );
 
 const clientProfileSchema = new mongoose.Schema(
-  {
-    companyName: String,
+    {
+        companyName: String,
 
-    industry: String,
+        industry: String,
 
-    description: String,
+        description: String,
 
-    website: String,
-  },
+        website: String,
+    },
 
-  { _id: false },
+    { _id: false },
 );
 
 const userSchema = new mongoose.Schema(
-  {
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
 
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
 
-    password: {
-      type: String,
-      required: true,
-      minlength: [8 , 'Password must be longer than 7 characters'],
-      select: false
-    },
+        password: {
+            type: String,
+            required: true,
+            minlength: [8, 'Password must be longer than 7 characters'],
+            select: false,
+        },
 
-    passwordConfirm: {
-        type: String,
-        required: true,
-        validate: {
-            validator(val){
-                return val === this.password;
+        passwordConfirm: {
+            type: String,
+            required: true,
+            validate: {
+                validator(val) {
+                    return val === this.password;
+                },
+
+                message: 'Passwords MUST match',
             },
+        },
 
-            message: "Passwords MUST match"
-        }
+        role: {
+            type: String,
+            enum: ['client', 'freelancer', 'admin'],
+            required: true,
+        },
+
+        phone: String,
+
+        profileImage: String,
+
+        freelancerProfile: freelancerProfileSchema,
+
+        clientProfile: clientProfileSchema,
+
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        passwordChangedAt: {
+            type: Date,
+        },
+
+        resetPasswordToken: String,
+
+        passwordResetExpires: Date,
+
+        active: {
+            type: Boolean,
+            default: true,
+            select: false,
+        },
     },
 
-    role: {
-      type: String,
-      enum: ['client', 'freelancer', 'admin'],
-      required: true,
-    },
-
-    phone: String,
-
-    profileImage: String,
-
-    freelancerProfile: freelancerProfileSchema,
-
-    clientProfile: clientProfileSchema,
-
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-
-    passwordChangedAt: {
-        type: Date,
-    },
-
-    resetPasswordToken: String,
-
-    passwordResetExpires: Date,
-
-  },
-
-  { timestamps: true },
+    { timestamps: true },
 );
 
-
-userSchema.methods.doPasswordsMatch = async function (plainPassword , hashedPassword){
-    return await bcrypt.compare(plainPassword , hashedPassword);
-}
-
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
-  if (this.passwordChangedAt) {
-    const changedTimestamp = parseInt(
-      this.passwordChangedAt.getTime() / 1000,
-      10
-    );
-
-    return JWTTimestamp < changedTimestamp;
-  }
-
-  return false;
+userSchema.methods.doPasswordsMatch = async function (plainPassword, hashedPassword) {
+    return await bcrypt.compare(plainPassword, hashedPassword);
 };
 
-userSchema.methods.createPasswordResetToken = function(){
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
     const resetToken = crypto.randomBytes(32).toString('hex');
 
     this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-    this.passwordResetExpires = Date.now() + 10 * 1000 * 60;
+    this.passwordResetExpires = Date.now() + 10 * 1000 * 60; // Expires in 10 minutes
 
     return resetToken;
 };
 
-
 // document middleware to hash password before saving it , to not save passwordConfirm
-userSchema.pre('save' , async function(){
+userSchema.pre('save', async function (next) {
     // if he has modified anything other than password , we don't wanna hash it again
-    if(!this.isModified("password")) return;
+    if (!this.isModified('password')) return;
 
     // other than than feel free to hash it (new user , modified password)
-    this.password = await bcrypt.hash(this.password , 12);
+    this.password = await bcrypt.hash(this.password, 12);
     this.passwordConfirm = undefined;
-})
-
-
-
-
-
-
-
+});
 
 module.exports = mongoose.model('User', userSchema);
