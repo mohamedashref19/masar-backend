04575cc573c2ca4031d121a34998105447bd7c13
Mohamed Ashraf <mohamedashref2003195@gmail.com>
2026-05-04 22:46:44 +0300

    complete frealancer search and profile apis

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const filterObject = require('./../utils/filterObj');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const Email = require('./../utils/email');
+const apiFeatures = require('./../utils/apiFeatures');
 
 // Modules
 const jwt = require('jsonwebtoken');
@@ -92,5 +93,59 @@ exports.updateMyPassword = catchAsync(async (req, res, next) => {
     return res.status(200).json({
         status: 'success',
         message: 'Password Changed successfully',
+    });
+});
+
+exports.getAllFreelancers = catchAsync(async (req, res, next) => {
+    const filter = { role: 'freelancer' };
+    let queryObj = { ...req.query };
+
+    if (queryObj.skills) {
+        queryObj['freelancerProfile.skills'] = queryObj.skills;
+        delete queryObj.skills;
+    }
+    if (queryObj.title) {
+        queryObj['freelancerProfile.title'] = queryObj.title;
+        delete queryObj.title;
+    }
+    if (queryObj.experienceLevel) {
+        queryObj['freelancerProfile.experienceLevel'] = queryObj.experienceLevel;
+        delete queryObj.experienceLevel;
+    }
+    if (queryObj.rating) {
+        queryObj['freelancerProfile.rating'] = queryObj.rating;
+        delete queryObj.rating;
+    }
+    if (queryObj.hourlyRate) {
+        queryObj['freelancerProfile.hourlyRate'] = queryObj.hourlyRate;
+        delete queryObj.hourlyRate;
+    }
+    const features = new apiFeatures(User.find(filter), queryObj)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+
+    const freelancers = await features.query;
+
+    res.status(200).json({
+        status: 'success',
+        results: freelancers.length,
+        data: { freelancers },
+    });
+});
+
+exports.getFreelancer = catchAsync(async (req, res, next) => {
+    const freelancer = await User.findOne({
+        _id: req.params.id,
+        role: 'freelancer',
+    }).select('-passwordChangedAt -__v');
+
+    if (!freelancer) {
+        return next(new AppError('No freelancer found with that ID', 404));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: { freelancer },
     });
 });
--- server.js
+++ server.js
@@ -6,19 +6,17 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<db_username>', process.env.DATABASE_USERNAME).replace(
-  '<db_password>',
-  process.env.DATABASE_PASSWORD,
+    '<db_password>',
+    process.env.DATABASE_PASSWORD,
 );
 
-
+mongoose.set('strictQuery', false);
 mongoose
-  .connect(DB)
-  .then(() => console.log('DB connection successful!'))
-  .catch((err) => console.log('DB connection FAILED!!', err));
-
-  
+    .connect(DB)
+    .then(() => console.log('DB connection successful!'))
+    .catch((err) => console.log('DB connection FAILED!!', err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
