bed346f7b04f8d64fa6c3545401a4a6c2293f96f
Mohamed Ashraf <mohamedashref2003195@gmail.com>
2026-06-11 18:44:18 +0300

    add fly.toml 2

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,7 @@ const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP, please try again in an hour!',
+    proxy: true,
 });
 app.use('/api', limiter);
 
